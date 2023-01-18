1b039c84d9371dec9b88210bdb066212f951e01b
Huzaifa-Khalid <99287277+Huzaifa-Khalid-2@users.noreply.github.com>
2023-01-18 04:00:19 +0000

    added js code

--- snake.js
+++ snake.js
@@ -6,7 +6,39 @@ class Vector {
   }
 
   subtract({x, y}) {
-    return new Vector(this.x -x, this.y - y)
+    return new Vector(this.x - x, this.y - y)
+  }
+
+  add({x, y}) {
+    return new Vector(this.x + x, this.y - y)
+  }
+
+  scaleBy(number) {
+    return new Vector(this.x * number, this.y * number)
+  }
+
+  length() {
+    return Math.hypot(this.x, this.y)
+  }
+
+  normalize () {
+    return this.scaleBy(1 / this.length())
+  }
+
+  isOpposite(vector) {
+    const { x, y } = this.add(vector)
+    return areEqual(x, 0) && areEqual(y, 0)
+  }
+
+  equalTo({ x, y }) {
+    return areEqual(this.x, x) && areEqual(this.y, y)
+  }
+}
+
+class Segment {
+  constructor(start, end) {
+    this.start = start
+    this.end = end
   }
 
 // #reion constants
